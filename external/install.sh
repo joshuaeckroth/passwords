0b73e142c455e3613f8f4423f3bf58657a4f0110
Joshua Eckroth <joshuaeckroth@gmail.com>
2023-12-21 11:07:04 -0500

    Updated install.sh

--- external/install.sh
+++ external/install.sh
@@ -1,13 +1,13 @@
-git clone https://github.com/majensen/libneo4j-client.git libneo4j-client-v4
-mkdir libneo4j-client-v4-install
-cd libneo4j-client-v4
-./autogen.sh
-export LDFLAGS="-L/opt/homebrew/opt/openssl@1.1/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"
-export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@1.1/lib/pkgconfig"
-./configure --prefix=`pwd`/../libneo4j-client-v4-install --disable-tools --disable-werror
-make install
-cd ..
+#git clone https://github.com/majensen/libneo4j-client.git libneo4j-client-v4
+#mkdir libneo4j-client-v4-install
+#cd libneo4j-client-v4
+#./autogen.sh
+#export LDFLAGS="-L/opt/homebrew/opt/openssl@1.1/lib"
+#export CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"
+#export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@1.1/lib/pkgconfig"
+#./configure --prefix=`pwd`/../libneo4j-client-v4-install --disable-tools --disable-werror
+#make install
+#cd ..
 git clone git@github.com:hashcat/hashcat.git
 cd hashcat && make obj/combined.NATIVE.a
 git clone https://github.com/antirez/rax.git
